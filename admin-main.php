8e85a369f5716d943befea80dec4e43f908dc49e
Luis Sanchez <sase.priv@gmail.com>
2019-05-06 12:05:23 -0400

    Pagina de login y menu, simplemente estatica

---
+++ admin-main.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Inicio</title>
+  </head>
+  <body>
+      <?php
+          include "menu-lateral.html"
+      ?>
+  </body>
+</html>
---
+++ ejemplo.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>ejemplo</title>
+    
+  </head>
+  <body>
+      <?php
+          include "menu-lateral.html";
+      ?>
+  </body>
+</html>
