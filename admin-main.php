3fd90d4abf669b2705e0b138dc97aa8a912c4ce3
Luis Sanchez <sase.priv@gmail.com>
2019-05-07 00:52:07 -0400

    se ha agregado la pagina admin-main y se ha arreglado parte del menu lateral, ademas se ha borrado un icnono inecesario

--- admin-main.php
+++ admin-main.php
@@ -3,10 +3,43 @@
   <head>
     <meta charset="utf-8">
     <title>Inicio</title>
+    <link rel="stylesheet" href="css_files/admin-main.css" type="text/css"/>
   </head>
-  <body>
+  <body id="body1">
+      <div id="avion">
+      </div>
       <?php
           include "menu-lateral.html"
       ?>
+      <div id="titulo">
+          <h1 id="titulo">Aerolinea Argenti</h1>
+      </div>
+
+      <div id="informacion">
+          <div id="texto-titulo-contenedor">
+              <h3 id="texto-titulo">INFORMACION</h2>
+          </div>
+          <div id="texto-informacion">
+
+              <table border="0" id="tabla-info">
+                  <tr>
+                    <td class="tabla-titulo">Usuario: </td>
+                    <td class="tabla-resul">Ejemplo Luisaleja</td>
+                  </tr>
+                  <tr>
+                    <td class="tabla-titulo">Nombre: </td>
+                    <td class="tabla-resul">Ejemplo Luis Sanchez</td>
+                  </tr>
+                  <tr>
+                    <td class="tabla-titulo">Cedula: </td>
+                    <td class="tabla-resul">Ejemplo 26781211</td>
+                  </tr>
+                  <tr>
+                    <td class="tabla-titulo">Cargo: </td>
+                    <td class="tabla-resul">Ejemplo emepleado</td>
+                  </tr>
+              </table>
+          </div>
+      </div>
   </body>
 </html>
