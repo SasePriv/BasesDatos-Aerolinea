5e95b7cdbb9f0b387aafc0d28408d35efebb73d1
Luis Sanchez <sase.priv@gmail.com>
2019-05-07 21:44:36 -0400

    Agregada pagina vuelos y buscar, con su css

---
+++ vuelos.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Vuelos</title>
+    <link rel="stylesheet" href="css_files/vuelos.css" type="text/css">
+  </head>
+  <body id="body1">
+      <?php
+          include "menu-lateral.html"
+      ?>
+  </body>
+</html>
