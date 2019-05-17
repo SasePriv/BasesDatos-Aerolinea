c5047712b5bb58f19a3267924db5968d65e5f814
Luis Sanchez <sase.priv@gmai.com>
2019-05-16 23:00:43 -0400

    comenzando con las funcionalidades en vuelos

--- vuelos.php
+++ vuelos.php
@@ -12,5 +12,28 @@
       <?php
           include "menu-lateral.html"
       ?>
+      <h1 id="h1-titulo">Vuelos</h1>
+
+      <div id="informacion">
+        <table border="0" id="tabla-info">
+          <tr id="titulo-row">
+            <td>Numero Vuelo</td>
+            <td>Codigo Avion</td>
+            <td>Ruta</td>
+            <td>Estado Vuelo</td>
+            <td>Fecha - Hora</td>
+          </tr>
+          <?php
+          
+          ?>
+          <tr id="info">
+            <td>1234</td>
+            <td>A122</td>
+            <td>Valencia</td>
+            <td>Despegando</td>
+            <td>10/08/2018 15:00</td>
+          </tr>
+        </table>
+      </div>
   </body>
 </html>
